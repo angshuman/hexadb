2fd0ad2dcd7127528c5ee232922e458f376aa647
Justin Emgarten <juste@microsoft.com>
2020-02-05 19:17:58 -0800

    Semantic types

---
+++ Hexastore.Test/DataGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Hexastore.Test
+{
+    public static class DataGenerator
+    {
+        public static JObject GenerateTelemetry(string deviceId, string property, double value)
+        {
+            return GenerateTelemetry(deviceId, DateTime.UtcNow, new Dictionary<string, double>() { { property, value } }, new Dictionary<string, string>());
+        }
+
+        public static JObject GenerateTelemetry(string deviceId, DateTimeOffset time, Dictionary<string, double> numberValues)
+        {
+            return GenerateTelemetry(deviceId, time, numberValues, new Dictionary<string, string>());
+        }
+
+        public static JObject GenerateTelemetry(string deviceId, DateTimeOffset time, Dictionary<string, double> numberValues, Dictionary<string, string> stringValues)
+        {
+            var json = GenerateEmptyMessage(deviceId, time);
+            var body = (JObject)json["data"]["Body"];
+
+            body.Add(numberValues.Select(e => new JProperty(e.Key, e.Value)));
+            body.Add(stringValues.Select(e => new JProperty(e.Key, e.Value)));
+
+            return json;
+        }
+
+        public static JObject GenerateEmptyMessage(string deviceId, DateTimeOffset time)
+        {
+            return new JObject(
+                new JProperty("id", deviceId),
+                new JObject("data", 
+                new JProperty("EnqueuedTimeUtc", time.ToString("o")),
+                new JProperty("Properties", new JObject()),
+                new JProperty("SystemProperties", new JObject(
+                    new JProperty("connectionDeviceId", deviceId))),
+                new JProperty("Body", new JObject())));
+        }
+    }
+}
---
+++ Hexastore.Test/SemanticTypeTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hexastore.Graph;
+using Hexastore.Parser;
+using Hexastore.Processor;
+using Hexastore.Resoner;
+using Hexastore.Rocks;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Hexastore.Test
+{
+    [TestClass]
+    public class SemanticTypeTest
+    {
+        [TestMethod]
+        public void SemanticType_BasicTest()
+        {
+            using (var testFolder = new TestFolder()) {
+                var factory = new LoggerFactory();
+                var logger = factory.CreateLogger<RocksGraphProvider>();
+                var storeLogger = factory.CreateLogger<StoreProcessor>();
+                var provider = new RocksGraphProvider(logger, testFolder);
+                var storeProvider = new SetProvider(provider);
+                var storeOperationFactory = new StoreOperationFactory();
+                var storeProcessor = new StoreProcessor(storeProvider, new Reasoner(), storeOperationFactory, storeLogger);
+                var storeId = "test";
+
+                // Populate telemetry
+                storeProcessor.PatchJson(storeId, DataGenerator.GenerateTelemetry("freezer-1", "Temperature", 1.0));
+
+                storeProcessor.PatchJson(storeId, DataGenerator.GenerateTelemetry("fridge-1", "Temperature", 1.1));
+                storeProcessor.PatchJson(storeId, DataGenerator.GenerateTelemetry("fridge-1", "FridgeTemperature", 5.0));
+
+                storeProcessor.PatchJson(storeId, DataGenerator.GenerateTelemetry("room-1", "Temperature", 20.0));
+
+                // Query for all temperatures for interface
+                
+            }
+        }
+    }
+}
