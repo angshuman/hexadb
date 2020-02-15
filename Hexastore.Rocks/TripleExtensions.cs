1dbc949ea3ad353007a53cc7e2fd8117b63077df
Angshuman Sarkar <asarkar@microsoft.com>
2020-02-15 14:12:36 -0800

    Reduce serialized data in the value

--- Hexastore.Rocks/TripleExtensions.cs
+++ Hexastore.Rocks/TripleExtensions.cs
@@ -8,6 +8,69 @@ namespace Hexastore.Rocks
 {
     public static class TripleExtensions
     {
+        public static Triple ToTriple(this (byte[], byte[]) input)
+        {
+            var key = input.Item1;
+            var value = input.Item2;
+
+            byte z = KeyConfig.ByteZero[0];
+            var firstZPos = Array.IndexOf(key, z);
+            string s;
+            string p;
+            string o;
+            int index;
+            bool isId;
+            JTokenType type;
+
+            int sEnd;
+            int pEnd;
+            int oEnd;
+            int isIdEnd;
+            int indexEnd;
+
+            switch (key[firstZPos - 1]) {
+                case KeyConfig.SMark:
+                    sEnd = Array.IndexOf(key, z, firstZPos + 1);
+                    s = Encoding.UTF8.GetString(key, firstZPos + 1, sEnd - firstZPos -1);
+                    pEnd = Array.IndexOf(key, z, sEnd + 1);
+                    p = Encoding.UTF8.GetString(key, sEnd + 1, pEnd - sEnd -1);
+                    index = BitConverter.ToInt32(key, pEnd + 1);
+                    var to = value.ToTripleObject();
+                    o = to.Value;
+                    isId = to.IsID;
+                    type = to.TokenType;
+                    break;
+                case KeyConfig.PMark:
+                    pEnd = Array.IndexOf(key, z, firstZPos + 1);
+                    p = Encoding.UTF8.GetString(key, firstZPos + 1,  pEnd - firstZPos -1);
+                    isId = key[pEnd + 1] == KeyConfig.ByteTrue[0] ? true : false;
+                    isIdEnd = pEnd + 2;
+                    oEnd = Array.IndexOf(key, z, isIdEnd + 1);
+                    o = Encoding.UTF8.GetString(key, pEnd + 3, oEnd - isIdEnd -1);
+                    index = BitConverter.ToInt32(key, oEnd + 1);
+                    indexEnd = oEnd + 5;
+                    s = Encoding.UTF8.GetString(key, indexEnd + 1, key.Length - indexEnd -1);
+                    type = (JTokenType)BitConverter.ToInt32(value, 0);
+                    break;
+                case KeyConfig.OMark:
+                    isIdEnd = firstZPos + 2;
+                    isId = key[firstZPos + 1] == KeyConfig.ByteTrue[0] ? true : false;
+                    oEnd = Array.IndexOf(key, z, isIdEnd + 1);
+                    o = Encoding.UTF8.GetString(key, isIdEnd + 1, oEnd - isIdEnd -1);
+                    sEnd = Array.IndexOf(key, z, oEnd + 1);
+                    s = Encoding.UTF8.GetString(key, oEnd + 1, sEnd - oEnd -1);
+                    pEnd = Array.IndexOf(key, z, sEnd + 1);
+                    p = Encoding.UTF8.GetString(key, sEnd + 1, pEnd - sEnd -1);
+                    index = BitConverter.ToInt32(key, pEnd + 1);
+                    type = (JTokenType)BitConverter.ToInt32(value, 0);
+                    break;
+                default:
+                    throw new InvalidOperationException("unmatched key. panic.");
+            }
+
+            return new Triple(s, p, new TripleObject(o, isId, type, index));
+        }
+
         public static byte[] ToBytes(this Triple triple)
         {
             var s = triple.Subject;
---
+++ Hexastore.Test/TripleObjectTest.cs
@@ -0,0 +1,31 @@
+﻿using Hexastore.Graph;
+using Hexastore.Rocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Hexastore.Test
+{
+    [TestClass]
+    public class TripleObjectTest
+    {
+        [TestMethod]
+        public void Convert_To_Byte_Roundtrip_Returns()
+        {
+            var o1 = new TripleObject("abc", true, JTokenType.String, null);
+            var bytes1 = o1.ToBytes();
+
+            var roundtripped = bytes1.ToTripleObject();
+            Assert.IsTrue(roundtripped.IsID);
+            Assert.AreEqual(o1.Value, roundtripped.Value);
+            Assert.AreEqual(o1.TokenType, roundtripped.TokenType);
+
+            var o2 = TripleObject.FromData(5);
+            var bytes2 = o2.ToBytes();
+
+            var roundtripped2 = bytes2.ToTripleObject();
+            Assert.IsFalse(roundtripped2.IsID);
+            Assert.AreEqual(o2.Value, roundtripped2.Value);
+            Assert.AreEqual(o2.TokenType, roundtripped2.TokenType);
+        }
+    }
+}
