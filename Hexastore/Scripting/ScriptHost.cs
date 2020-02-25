9a450ed185d0bcf58dd6c5493015c397c107127d
Angshuman Sarkar <sendtoansu@gmail.com>
2020-02-25 14:10:06 -0800

    wip

---
+++ Hexastore/Scripting/ScriptHost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hexastore.Processor;
+using NLua;
+
+namespace Hexastore.Scripting
+{
+    public class ScriptHost : IDisposable
+    {
+        private readonly Lua _lua;
+
+        public ScriptHost(IStoreProcesor storeProcessor)
+        {
+            _lua = new Lua();
+            _lua["store"] = storeProcessor;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hexastore/Scripting/StoreAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hexastore.Scripting
+{
+    public class StoreAccess
+    {
+        public StoreAccess(string storeId)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
