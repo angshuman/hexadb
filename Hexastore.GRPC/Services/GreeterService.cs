f7c6fea94327793f4b82f70e7dd6cab3b321928a
Angshuman Sarkar <asarkar@microsoft.com>
2022-05-06 19:12:07 -0700

    Update to net5.0. Add protobuf server

---
+++ Hexastore.GRPC/Services/GreeterService.cs
@@ -0,0 +1,23 @@
+﻿namespace Hexastore.GRPC
+{
+    using System.Threading.Tasks;
+    using Grpc.Core;
+    using Microsoft.Extensions.Logging;
+
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ Hexastore.GRPC/Services/IngestService.cs
@@ -0,0 +1,24 @@
+﻿namespace Hexastore.GRPC.Services
+{
+    using System.Threading.Tasks;
+    using Grpc.Core;
+    using Microsoft.Extensions.Logging;
+
+    public class IngestService : Ingest.IngestBase
+    {
+        private readonly ILogger<IngestService> _logger;
+
+        public IngestService(ILogger<IngestService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AssertResponse> Assert(AssertRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new AssertResponse {
+                Message = "Done",
+                Count = 1
+            });
+        }
+    }
+}
